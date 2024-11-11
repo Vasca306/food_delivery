857157ab28c5672f6325655563e94f2a96a9de94
Vasca306 <buitan0164@gmail.com>
2024-11-11 20:35:13 +0700

    tan

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -49,7 +49,7 @@ const List = () => {
         {list.map((item, index) => {
           return (
             <div key={index} className='list-table-format'>
-              <img src={`${url}/images` + item.image} alt="" />
+              <img src={`${url}/images/` + item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{item.price}.000VND</p>
